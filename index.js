abd475cb3ccd7276e09c829465da799137768569
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-08-01 11:11:21 +0200

    Realized i was using let instead of this.branchInfo which was causing issues

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-let bankBranchInstance = BankBranch;
+let bankBranchInstance = null;
 // 2. Define a class called `BankBranch` for managing branch information.
 class BankBranch {
   constructor(branchInfo) {
     if (bankBranchInstance === null) {
       this.branchInfo = bankBranchInstance;
-    } else return bankBranchInstance;
+    } else {
+      return bankBranchInstance;
+    }
   }
 }
+
+const bankBranchCheck = BankBranch;
+console.log(bankBranchCheck);
 
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
